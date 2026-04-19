8b61e6ba8f78fdbe711c06d672f835aa665c1d58
nora-code-27 <nballant@ucsc.edu>
2026-04-18 18:12:11 -0700

    Add files via upload

--- Journal1-Prompt1.py
+++ Journal1-Prompt1.py
@@ -9,7 +9,9 @@ Created on Tue Apr  7 12:37:29 2026
 def main():
     print("Name: Nora Ballantyne")
     print("Preffered Pronouns: She/her")
+# %%
     print("Favorite movie: Howl's Moving Castle \nbecause I love the artstyle and the voice acting")
+
     print("Favorite food: Sushi becasue it feels very fresh")
     
 if __name__ == "__main__":
